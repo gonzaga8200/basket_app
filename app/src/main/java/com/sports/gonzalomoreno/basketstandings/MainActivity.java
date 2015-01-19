0a79b5fb95a5c0a13f63e428aecf3f9f7090de4d
gonzaga8200 <gonzaga8200@gmail.com>
2015-01-19 19:29:25 +0100

    Create Edit Text

--- app/src/main/java/com/sports/gonzalomoreno/basketstandings/MainActivity.java
+++ app/src/main/java/com/sports/gonzalomoreno/basketstandings/MainActivity.java
@@ -4,14 +4,32 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText playerList;
+    ListView upPlayers;
+    Button buttonAddPlayer;
+    private List<String> supplierNames1 = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playerList = (EditText) findViewById(R.id.lista_jugadores);
+        upPlayers = (ListView) findViewById(R.id.jugadores_aptos);
+        buttonAddPlayer = (Button) findViewById(R.id.boton_add);
+        buttonAddPlayer.setOnClickListener(null);
+
     }
 
 

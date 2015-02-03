89af6509597858a2bfa8f112c594edd5c2f2928a
gonzaga8200 <gonzaga8200@gmail.com>
2015-02-03 22:43:34 +0100

    Fill players in GameActivity

--- app/src/main/java/com/sports/gonzalomoreno/basketstandings/GameActivity.java
+++ app/src/main/java/com/sports/gonzalomoreno/basketstandings/GameActivity.java
@@ -5,35 +5,65 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 
 public class GameActivity extends ActionBarActivity {
     String[] players;
     ListView listMatch;
     TableLayout dataTable;
+    //int [][] standingsBoard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         dataTable = (TableLayout) findViewById(R.id.tablaDatos);
-
+        ArrayList<Button> playerInfo = new ArrayList<Button>();
         Bundle extras = getIntent().getExtras();
         if (extras != null){
             players = extras.getStringArray("player_list");
+            /*standingsBoard = new int [10][players.length];
+            for (int i=0; i < 10; i++)
+                for (int j=0; j < players.length; i++)
+                    standingsBoard[i][j] = 0;*/
             for (int i=0; i < players.length; i++){
+                //final int playerNumber = i;
+
                 TableRow row = new TableRow(this);
                 TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
                 row.setLayoutParams(lp);
 
-                TextView myPlayer = new TextView(this);
+                final TextView myPlayer = new TextView(this);
                 myPlayer.setTextColor(Color.parseColor("#D8D8D8"));
                 myPlayer.setText(players[i]);
+
+                final Button twoPointsScore = new Button(this);
+                twoPointsScore.setId(i);
+                twoPointsScore.setText("Pulsa aquí");
+                twoPointsScore.setBackgroundColor(Color.parseColor("#FF5610"));
+                twoPointsScore.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //standingsBoard[0][playerNumber] += 2;
+                        myPlayer.setText("pocoyo");
+                        //twoPointsScore.setText(standingsBoard[0][playerNumber]);
+                    }
+                });
+                //playerInfo.add(new Button(this));
+
                 row.addView(myPlayer);
+                row.addView(twoPointsScore);
+
+
+
+
 
                 dataTable.addView(row,i);
             }

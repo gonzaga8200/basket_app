4743d0b8b0ff952719994c5158f4731d373fd0a8
gonzaga8200 <gonzaga8200@gmail.com>
2015-02-18 01:13:24 +0100

    Set three and one points

--- app/src/main/java/com/sports/gonzalomoreno/basketstandings/GameActivity.java
+++ app/src/main/java/com/sports/gonzalomoreno/basketstandings/GameActivity.java
@@ -12,8 +12,6 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
 
 public class GameActivity extends ActionBarActivity {
     String[] players;
@@ -49,24 +47,82 @@ public class GameActivity extends ActionBarActivity {
                 myPlayerStanding.setTextColor(Color.parseColor("#D8D8D8"));
                 myPlayerStanding.setText(players[i]);
 
-                final Button percentagePoints = new Button(this);
-                percentagePoints.setId(i);
-                percentagePoints.setText("0/0");
+                final Button percentage2Points = new Button(this);
+                percentage2Points.setId(i);
+                percentage2Points.setText("0/0");
+
+                final Button percentage1Points = new Button(this);
+                percentage1Points.setId(i);
+                percentage1Points.setText("0/0");
+
+                final Button percentage3Points = new Button(this);
+                percentage3Points.setId(i);
+                percentage3Points.setText("0/0");
 
 
                 final Button twoPointsScore = new Button(this);
+
+                twoPointsScore.setTextColor(Color.parseColor("#FFFFFF"));
+                twoPointsScore.setShadowLayer(5,0,0,Color.parseColor("#A8A8A8"));
+                twoPointsScore.setBackgroundColor(Color.GREEN);
+
+                final Button onePointsScore = new Button(this);
+
+                onePointsScore.setTextColor(Color.parseColor("#FFFFFF"));
+                onePointsScore.setShadowLayer(5,0,0,Color.parseColor("#A8A8A8"));
+                onePointsScore.setBackgroundColor(Color.GREEN);
+
+                final Button threePointsScore = new Button(this);
+
+                threePointsScore.setTextColor(Color.parseColor("#FFFFFF"));
+                threePointsScore.setShadowLayer(5,0,0,Color.parseColor("#A8A8A8"));
+                threePointsScore.setBackgroundColor(Color.GREEN);
+
+                /** TWO POINTS SCORE **/
+
                 twoPointsScore.setId(i);
                 twoPointsScore.setText("2p V");
-                twoPointsScore.setBackgroundColor(Color.parseColor("#FF5610"));
                 twoPointsScore.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         standingsBoard[0][playerNumber] += 1;
                         String totalThrows = Integer.toString(standingsBoard[0][playerNumber] + standingsBoard[1][playerNumber]);
                         String totalScore =  Integer.toString(standingsBoard[0][playerNumber]);
-                        percentagePoints.setText(totalScore + '/' + totalThrows);
+                        percentage2Points.setText(totalScore + '/' + totalThrows);
                     }
                 });
+
+                /** ONE POINTS SCORE **/
+
+                onePointsScore.setId(i);
+                onePointsScore.setText("1p V");
+                onePointsScore.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        standingsBoard[2][playerNumber] += 1;
+                        String totalThrows = Integer.toString(standingsBoard[2][playerNumber] + standingsBoard[3][playerNumber]);
+                        String totalScore =  Integer.toString(standingsBoard[2][playerNumber]);
+                        percentage1Points.setText(totalScore + '/' + totalThrows);
+                    }
+                });
+
+                /** THREE POINTS SCORE **/
+
+                threePointsScore.setId(i);
+                threePointsScore.setText("3p V");
+                threePointsScore.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        standingsBoard[4][playerNumber] += 1;
+                        String totalThrows = Integer.toString(standingsBoard[4][playerNumber] + standingsBoard[5][playerNumber]);
+                        String totalScore =  Integer.toString(standingsBoard[4][playerNumber]);
+                        percentage3Points.setText(totalScore + '/' + totalThrows);
+                    }
+                });
+
+                /******* FAILED *******/
+
+                /** TWO POINTS **/
 
                 final Button twoPointsFailed = new Button(this);
                 twoPointsFailed.setId(i);
@@ -78,15 +134,53 @@ public class GameActivity extends ActionBarActivity {
                         standingsBoard[1][playerNumber] += 1;
                         String totalThrows = Integer.toString(standingsBoard[0][playerNumber] + standingsBoard[1][playerNumber]);
                         String totalScore =  Integer.toString(standingsBoard[0][playerNumber]);
-                        percentagePoints.setText(totalScore + '/' + totalThrows);
+                        percentage2Points.setText(totalScore + '/' + totalThrows);
+                    }
+                });
+
+                /** ONE POINTS **/
+
+                final Button onePointsFailed = new Button(this);
+                onePointsFailed.setId(i);
+                onePointsFailed.setText("1p X");
+                onePointsFailed.setBackgroundColor(Color.parseColor("#FF5610"));
+                onePointsFailed.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        standingsBoard[3][playerNumber] += 1;
+                        String totalThrows = Integer.toString(standingsBoard[2][playerNumber] + standingsBoard[3][playerNumber]);
+                        String totalScore =  Integer.toString(standingsBoard[2][playerNumber]);
+                        percentage1Points.setText(totalScore + '/' + totalThrows);
+                    }
+                });
+
+                /** THREE POINTS **/
+
+                final Button threePointsFailed = new Button(this);
+                threePointsFailed.setId(i);
+                threePointsFailed.setText("3p X");
+                threePointsFailed.setBackgroundColor(Color.parseColor("#FF5610"));
+                threePointsFailed.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        standingsBoard[5][playerNumber] += 1;
+                        String totalThrows = Integer.toString(standingsBoard[4][playerNumber] + standingsBoard[5][playerNumber]);
+                        String totalScore =  Integer.toString(standingsBoard[4][playerNumber]);
+                        percentage3Points.setText(totalScore + '/' + totalThrows);
                     }
                 });
 
                 row.addView(myPlayer);
                 row.addView(twoPointsScore);
                 row.addView(twoPointsFailed);
+                row.addView(onePointsScore);
+                row.addView(onePointsFailed);
+                row.addView(threePointsScore);
+                row.addView(threePointsFailed);
                 rowStandings.addView(myPlayerStanding);
-                rowStandings.addView(percentagePoints);
+                rowStandings.addView(percentage2Points);
+                rowStandings.addView(percentage1Points);
+                rowStandings.addView(percentage3Points);
 
                 dataTable.addView(row,i);
                 dataTable2.addView(rowStandings,i);

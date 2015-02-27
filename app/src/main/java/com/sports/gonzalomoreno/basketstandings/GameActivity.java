bbfd90b60670bf919472c1a3842f58c17f8ce578
gonzaga8200 <gonzaga8200@gmail.com>
2015-02-27 09:22:46 +0100

    Drag and drop funcionality

--- app/src/main/java/com/sports/gonzalomoreno/basketstandings/GameActivity.java
+++ app/src/main/java/com/sports/gonzalomoreno/basketstandings/GameActivity.java
@@ -1,15 +1,22 @@
 package com.sports.gonzalomoreno.basketstandings;
 
+import android.content.ClipData;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -24,16 +31,61 @@ public class GameActivity extends ActionBarActivity {
     TextView [] players = new TextView[5];
 
     public static int [][] standingsBoard ;
+
+    private final class MyTouchListener implements View.OnTouchListener {
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                view.startDrag(data, shadowBuilder, null, 0);
+                //view.setVisibility(View.INVISIBLE);
+                return false;
+
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         players[0] = (TextView) findViewById(R.id.jugador1);
+        class MyDragListener implements View.OnDragListener {
+            Drawable enterShape = getResources().getDrawable(R.drawable.players);
+            Drawable normalShape = getResources().getDrawable(R.drawable.players);
+
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                int action = event.getAction();
+                switch (event.getAction()) {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        // do nothing
+                        break;
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        //players[0].setText("pirikichi");
+                        break;
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        //v.setBackground(normalShape);
+                        break;
+                    case DragEvent.ACTION_DROP:
+                        // Dropped, reassign View to ViewGroup
+                        break;
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        startingLineup.get(0).set2pointsScored();
+                        String points = Integer.toString(startingLineup.get(0).getTotalPoints());
+                        players[0].setText(points);
+                    default:
+                        break;
+                }
+                return true;
+            }
+        }
+        players[0].setOnDragListener(new MyDragListener());
         players[1] = (TextView) findViewById(R.id.jugador2);
         players[2] = (TextView) findViewById(R.id.jugador3);
         players[3] = (TextView) findViewById(R.id.jugador4);
         players[4] = (TextView) findViewById(R.id.jugador5);
         scored2 = (TextView) findViewById(R.id.scored_two);
+        scored2.setOnTouchListener(new MyTouchListener());
+
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
